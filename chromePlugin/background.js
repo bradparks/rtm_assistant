15c9f983acf61487b5ef5129472a33e44467c5de
chivorotkiv <shmakov@omich.net>
2014-09-07 23:31:52 +0700

    [20] -- use chrono.js to detect due date from text.

--- chromePlugin/background.js
+++ chromePlugin/background.js
@@ -1,9 +1,10 @@
-var openRtmWindow = function(tab, selection)
+var openRtmWindow = function(tab, selection, date)
 {
 	var name = selection || tab.title
 
 	var rtmUrl='http://m.rememberthemilk.com/add?name=' +
-		encodeURIComponent(name) + '&url=' + encodeURIComponent(tab.url)
+		encodeURIComponent(name) + '&url=' + encodeURIComponent(tab.url) +
+		'&due=' + date
 
 	window.open(rtmUrl, 'addwindow',
 		'status=no,toolbar=no,width=250,height=560,resizable=yes')
@@ -14,6 +15,8 @@ chrome.browserAction.onClicked.addListener(function(tab)
 	chrome.tabs.executeScript(tab.id, {file: "bookmarklet.js"},
 		function(result)
 		{
-			openRtmWindow(tab, result && result[0])
+			var text = result && result[0]
+			var date = text && chrono.parseDate(text)
+			openRtmWindow(tab, text, date)
 		})
 });
